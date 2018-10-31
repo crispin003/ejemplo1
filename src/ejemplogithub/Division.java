3f76204f89bda7aa492455076b92f4e533029b41
harold <harold_r008@hotmail.com>
2018-10-30 20:39:54 -0500

    multiplicacion y division

---
+++ src/ejemplogithub/Division.java
@@ -0,0 +1,12 @@
+package ejemplogithub;
+
+public class Division {
+
+    public int division(int dividendo, int divisor) {
+        return dividendo / divisor;
+    }
+
+    public int residuo(int dividendo, int divisor) {
+        return dividendo % divisor;
+    }
+}
